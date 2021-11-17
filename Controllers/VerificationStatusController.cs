456e1b29bd7e6775bebc93843367a072bee81eff
FullOfDelirium <91204498+FullOfDelirium@users.noreply.github.com>
2021-11-17 20:57:34 +0300

    .

---
+++ Controllers/VerificationStatusController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Employment.Controllers
+{
+    [ApiController]
+    [Route("/Vacancy")]
+    public class VerificationStatusController : ControllerBase
+    {
+        [HttpPost]
+        public string Create(string str)
+        {
+            return str;
+        }
+
+        [HttpGet]
+        public string Read(string str)
+        {
+            return str;
+        }
+
+        [HttpPut]
+        public string Update(string str)
+        {
+            return str;
+        }
+
+        [HttpDelete]
+        public string Delete(string str)
+        {
+            return str;
+        }
+    }
+}
